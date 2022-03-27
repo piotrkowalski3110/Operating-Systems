d37ac15105fd855c7bb37522423d2f1ad68226fc
Piotr Kowalski <piotr.kowalski3110@gmail.com>
2022-03-27 15:16:49 +0200

    zmiany w FIFO

--- lab5/zad9/klient.c
+++ lab5/zad9/klient.c
@@ -5,22 +5,40 @@
 #include <unistd.h>
 #include <string.h>
 
-#define FIFO_FILE "fifo_oneway"
+#define FIFO_FILE "50901"
 
-int fd;
-char send[20];
+   int stream;
+   int status;
+   int len;
+   char send[20];
+   char end[10];
 
-int main() 
-{
-   mknod(FIFO_FILE, __S_IFIFO|0640, 0);
+int main() {
+
+   stream = open(FIFO_FILE, O_WRONLY);
+   strcpy(end, "50901");
 
    for(;;)
    {
-      fd = open(FIFO_FILE, O_WRONLY);
-      printf("Message for server: ");
-      fgets(send, 20, stdin);
-      write(fd, send, strlen(send)+1);
-      close(fd);
+      printf("Send message to server: ");
+      fgets(send, sizeof(send), stdin);
+
+      len = strlen(send);
+      send[len - 1] = '\0';
+      status = strcmp(send, end);
+
+      if (status != 0)
+      {
+         write(stream, send, strlen(send));
+      }
+
+      else
+      {
+         write(stream, send, strlen(send));
+         close(stream);
+         printf("\nENDING...\n");
+         break;
+      }
    }
    return 0;
 }
--- lab5/zad9/server.c
+++ lab5/zad9/server.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
-#define FIFO_FILE "fifo_oneway"
+#define FIFO_FILE "50901"
 
-int fd;
-char receive[20];
+   int stream;
+   char receive[80];
+   char end[10];
+   int status;
+   int len;
 
-int main()
-{
+int main() {
 
    mknod(FIFO_FILE, __S_IFIFO|0640, 0);
+   strcpy(end, "50901");
+   stream = open(FIFO_FILE, O_RDONLY);
 
-   for(;;)
-   {
-      fd = open(FIFO_FILE,O_RDONLY);
-      read(fd, receive, 20);
-      printf("Message from klient: %s", receive);
-      close(fd);
+   for(;;){
+      len = read(stream, receive, sizeof(receive));
+      receive[len] = '\0';
+      printf("Message from client: %s\n", receive);
+
+      status = strcmp(receive, end);
+      if (status == 0) {
+         close(stream);
+         printf("\nENDING...\n");
+         break;
+      }
    }
-    return 0;
+   return 0;
 }
