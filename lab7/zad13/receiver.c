8b20a54fedcd59be58b77ae5f271fc42bf4e5758
Piotr Kowalski <piotr.kowalski3110@gmail.com>
2022-04-03 20:26:12 +0200

    pamiec wspoldzielona

---
+++ lab7/zad13/receiver.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<string.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+#define BUF_SIZE 1024
+#define SHM_KEY 0x1234
+
+struct shmseg {
+   int cnt;
+   int complete;
+   char buf[BUF_SIZE];
+};
+
+int main(int argc, char *argv[]) {
+   int shmid;
+   char *bufptr;
+   int spaceavailable;
+   struct shmseg *shmp;
+   shmid = shmget(SHM_KEY, sizeof(struct shmseg), 0644|IPC_CREAT);
+   shmp = shmat(shmid, NULL, 0);
+
+   while (shmp->complete != 1)
+   {
+      bufptr = shmp->buf;
+      spaceavailable = BUF_SIZE;
+      
+      if(strlen(shmp->buf)>0)
+      {
+      printf("segment contains : %s", shmp->buf);
+      printf("Reading Process: Shared Memory: Read %d bytes\n\n", shmp->cnt);
+      }
+      strcpy(bufptr, "");
+      bufptr[spaceavailable-1] = '\0';
+      sleep(1);
+   }
+   printf("\nReading Process: Complete\n");
+   shmdt(shmp);
+   shmctl(shmid, IPC_RMID, 0);
+   return 0;
+}
---
+++ lab7/zad13/sender.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<string.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+#define BUF_SIZE 1024
+#define SHM_KEY 0x1234
+
+struct shmseg {
+   int cnt;
+   int complete;
+   char buf[BUF_SIZE];
+};
+
+int main(int argc, char *argv[]) {
+
+   char *message = NULL;
+   size_t len = 0;
+   int filled_count;
+
+   int shmid;
+   struct shmseg *shmp;
+   char *bufptr;
+   int spaceavailable;
+   shmid = shmget(SHM_KEY, sizeof(struct shmseg), 0644|IPC_CREAT);
+   shmp = shmat(shmid, NULL, 0);
+
+   while((getline(&message, &len, stdin)) != -1)
+   {
+      bufptr = shmp->buf;
+      spaceavailable = BUF_SIZE;
+
+      strcpy(bufptr, message);
+      bufptr[spaceavailable-1] = '\0';
+
+      shmp->cnt = strlen(bufptr);
+
+      bufptr[spaceavailable-1] = '\0';
+      shmp->complete = 0;
+      printf("Wrote %d bytes\n\n", shmp->cnt);
+   }
+   shmp->complete = 1;
+   free(message);
+   shmdt(shmp);
+   return 0;
+}
