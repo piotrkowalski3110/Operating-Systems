96949215dc7aa12b5182bbfffcf39725994ff309
Piotr Kowalski <piotr.kowalski3110@gmail.com>
2022-04-04 13:06:27 +0200

    poprawka

--- lab7/zad14/receiver.c
+++ lab7/zad14/receiver.c
@@ -7,15 +7,22 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<string.h>
+#include <signal.h>
 
 #define BUF_SIZE 1024
 #define SHM_KEY 0x1234
+int flag;
 
 struct shmseg {
    int cnt;
    int complete;
    char buf[BUF_SIZE];
 };
+
+void sig_handler(int signum)
+{
+    flag = 1;
+}
 
 int main(int argc, char *argv[]) {
    int shmid;
@@ -24,19 +31,19 @@ int main(int argc, char *argv[]) {
    struct shmseg *shmp;
    shmid = shmget(SHM_KEY, sizeof(struct shmseg), 0644|IPC_CREAT);
    shmp = shmat(shmid, NULL, 0);
+   signal(SIGUSR1, sig_handler);
+
+    int pid_t = getpid();
+    printf("Moj PID to: %d\n", pid_t);
 
    while (shmp->complete != 1)
    {
-      bufptr = shmp->buf;
-      spaceavailable = BUF_SIZE;
-      
-      if(strlen(shmp->buf)>0)
+      if(flag == 1)
       {
       printf("segment contains : %s", shmp->buf);
       printf("Reading Process: Shared Memory: Read %d bytes\n\n", shmp->cnt);
       }
-      strcpy(bufptr, "\0");
-      //bufptr[spaceavailable-1] = '\0';
+      flag = 0;
       sleep(1);
    }
    printf("\nReading Process: Complete\n");
---
+++ lab7/zad14/sender.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<string.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include <signal.h>
+
+#define BUF_SIZE 1024
+#define SHM_KEY 0x1234
+
+struct shmseg {
+   int cnt;
+   int complete;
+   char buf[BUF_SIZE];
+};
+
+int main(int argc, char *argv[]) {
+
+   char *message = NULL;
+   size_t len = 0;
+   int filled_count;
+
+   int shmid;
+   struct shmseg *shmp;
+   char *bufptr;
+   int spaceavailable;
+   shmid = shmget(SHM_KEY, sizeof(struct shmseg), 0644|IPC_CREAT);
+   shmp = shmat(shmid, NULL, 0);
+   bufptr = shmp->buf;
+   spaceavailable = BUF_SIZE;
+
+   printf("Enter receiver PID: ");
+   int pid_t;
+   scanf("%d", &pid_t);
+
+   while((getline(&message, &len, stdin)) != -1)
+   {
+      strcpy(bufptr, message);
+      bufptr[spaceavailable-1] = '\0';
+      shmp->cnt = strlen(bufptr);
+      shmp->complete = 0;
+
+      printf("Wrote %d bytes\n\n", shmp->cnt);
+      kill(pid_t, SIGUSR1);
+   }
+   shmp->complete = 1;
+   free(message);
+   shmdt(shmp);
+   return 0;
+}
